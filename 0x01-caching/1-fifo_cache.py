112ccc582e60ac25179b76cecef8cd53f3094842
3zzazakl <vanzekovichgr@gmail.com>
2025-01-25 21:06:17 +0200

    fixing errr

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -41,7 +41,7 @@ class FIFOCache(BaseCaching):
                 if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
                     first = self.queue.pop(0)
                     del self.cache_data[first]
-                    print("DISCARDING {}".format(first))
+                    print("DISCARD: {}".format(first))
                 self.cache_data[key] = item
             self.queue.append(key)
 

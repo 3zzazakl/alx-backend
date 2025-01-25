921bf9a9c3d0dfbeb76ed2a4a688e541fd476dee
3zzazakl <vanzekovichgr@gmail.com>
2025-01-25 21:10:00 +0200

    setting files

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+"""summary_line
+
+Keyword arguments:
+argument -- description
+Return: return_description
+"""
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """summary_line
+
+    Keyword arguments:
+    argument -- description
+    Return: return_description
+    """
+
+    def __init__(self):
+        """summary_line
+
+        Keyword arguments:
+        argument -- description
+        Return: return_description
+        """
+        super().__init__()
+        self.queue = []
+
+    def put(self, key, item):
+        """summary_line
+
+        Keyword arguments:
+        argument -- description
+        Return: return_description
+        """
+        if key and item:
+            if key in self.cache_data:
+                self.cache_data[key] = item
+                self.queue.remove(key)
+            else:
+                if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                    last = self.queue.pop()
+                    del self.cache_data[last]
+                    print("DISCARD: {}".format(last))
+                self.cache_data[key] = item
+            self.queue.append(key)
+
+    def get(self, key):
+        """summary_line
+
+        Keyword arguments:
+        argument -- description
+        Return: return_description
+        """
+        if key in self.cache_data:
+            self.queue.remove(key)
+            self.queue.append(key)
+            return self.cache_data[key]
+        return None

5b60aa689936edf69380b74f3d58ede8fa9678e4
3zzazakl <vanzekovichgr@gmail.com>
2025-01-26 03:33:52 +0200

    checking flask

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,10 +7,10 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def index():
+def index() -> str:
     """Renders the index page"""
     return render_template('0-index.html')
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port="5000", host="0.0.0.0", debug=True)
